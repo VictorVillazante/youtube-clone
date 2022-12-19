770bd8995e9901dbba960449c34f94d00246741b
VictorVillazante <villazante.bernabe.victor@gmail.com>
2022-12-19 18:21:38 -0400

    put header on youtube clone

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Constant from 'expo-constants';
+import { AntDesign,Entypo,FontAwesome,MaterialIcons } from '@expo/vector-icons'; 
+export function Header() {
+  const mycolor="#212121"
+  return (
+    <View style={{
+        marginTop: Constant.statusBarHeight,
+        height: 45,
+        elevation:4,
+        backgroundColor:"white",
+        flexDirection:"row",
+        justifyContent:"space-between"
+    }}
+    >
+        <View
+            style={{
+                margin:5,
+                flexDirection:"row"
+            }}
+        >
+            <AntDesign name="youtube" size={24} color="red" style={{marginLeft:20}} />
+            <Text style={{fontSize:22,marginLeft:5,fontWeight:"bold",color:mycolor}}>YouTube</Text>
+        </View>
+        <View
+            style={{
+                flexDirection:"row",
+                justifyContent:"space-around",
+                width:150,
+                marginTop:5
+            }}
+        >
+            <Entypo name="video-camera" size={24} color="black" />
+            <FontAwesome name="search" size={24} color="black" />
+            <MaterialIcons name="account-circle" size={24} color="black" />
+        </View>
+    </View>
+  );
+}
